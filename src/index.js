3ade734180abe43cff66cf7b7f83a4c5d3c9f4f9
IsabelaSouto <belasoutomaior@gmail.com>
2024-02-07 16:14:52 -0300

    req 2 done

--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@ app.use(express.json());
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
 
+// não remova esse endpoint, e para o avaliador funcionar
+app.get('/', (_request, response) => {
+  response.status(HTTP_OK_STATUS).send();
+});
+
 app.get('/talker', async (_req, res) => {
   try {
     const data = await fs.readFile(path.resolve(__dirname, 'talker.json'));
@@ -19,9 +24,16 @@ app.get('/talker', async (_req, res) => {
   }
 });
 
-// não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  
+  const data = await fs.readFile(path.resolve(__dirname, 'talker.json'));
+  const talkers = JSON.parse(data);
+  const talker = talkers.find((t) => t.id === Number(id));
+
+  if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+
+  return res.status(200).json(talker);
 });
 
 app.listen(PORT, () => {
