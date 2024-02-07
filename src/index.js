6bc2acdeb57a14986f180d92f4b4c0de6cef6d2d
IsabelaSouto <belasoutomaior@gmail.com>
2024-02-07 14:49:22 -0300

    req 1 done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
-// começando!!
+
+app.get('/talker', async (_req, res) => {
+  try {
+    const data = await fs.readFile(path.resolve(__dirname, 'talker.json'));
+    const talkers = JSON.parse(data);
+
+    return res.status(200).json(talkers);
+  } catch (err) {
+    return res.status(200).json([]);
+  }
+});
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
